f6fe3e370119cc3f6d3054f83764c060ac588d43
ptkmc <23647036+ptkmc@users.noreply.github.com>
2019-05-11 19:15:08 -0500

    Updates

---
+++ precache-manifest.13699fe29c1c72cf9e2fd7762cd3ed1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "281bab1416950016487b",
+    "url": "/video-search/static/js/runtime~main.281bab14.js"
+  },
+  {
+    "revision": "a61e53135a7189f4c435",
+    "url": "/video-search/static/js/main.a61e5313.chunk.js"
+  },
+  {
+    "revision": "81e52b1cf238e116290f",
+    "url": "/video-search/static/js/1.81e52b1c.chunk.js"
+  },
+  {
+    "revision": "a61e53135a7189f4c435",
+    "url": "/video-search/static/css/main.94005b2a.chunk.css"
+  },
+  {
+    "revision": "b606e5c637f23cd6132d2a185bca858e",
+    "url": "/video-search/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/video-search/precache-manifest.13699fe29c1c72cf9e2fd7762cd3ed1b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/video-search/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
