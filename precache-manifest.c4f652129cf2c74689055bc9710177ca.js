707251fff7f11e6592a4fd867406e79049849037
ptkmc <23647036+ptkmc@users.noreply.github.com>
2019-11-19 19:52:21 -0600

    Updates

---
+++ precache-manifest.c4f652129cf2c74689055bc9710177ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6170c8145d14a4d45ace6e971dc31084",
+    "url": "/video-search/index.html"
+  },
+  {
+    "revision": "7e18198ecb5f71c07387",
+    "url": "/video-search/static/css/main.e9488b38.chunk.css"
+  },
+  {
+    "revision": "b992b347d9bbe1d393cf",
+    "url": "/video-search/static/js/2.6dc00e61.chunk.js"
+  },
+  {
+    "revision": "7e18198ecb5f71c07387",
+    "url": "/video-search/static/js/main.cf5c09c9.chunk.js"
+  },
+  {
+    "revision": "281a4fbf35eb2d12be01",
+    "url": "/video-search/static/js/runtime-main.9cda6f2c.js"
+  }
+]);
